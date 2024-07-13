2bd80e007130ad743b2e088f49652436a19e94d4
Vallydei <obedkova.v@rambler.ru>
2024-07-13 20:09:02 +0300

    something done

--- src/__tests__/game.test.js
+++ src/__tests__/game.test.js
@@ -1,69 +1,28 @@
-class Character {
-  constructor(name, type) {
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-  }
+import Character from "../Character";
 
-  set name(value) {
-    if (value.length < 2 || value.length > 10 || typeof value !== "string") {
-      throw new Error("wrong name");
-    }
-    this._name = value;
-  }
+let hero;
 
-  get name() {
-    return this._name;
-  }
+beforeEach(() => {
+  hero = new Character("OOP", "Magician");
+});
 
-  set type(value) {
-    if (value == "Bowman") {
-      this.attack = 25;
-      this.defence = 25;
-    } else if (value == "Swordsman") {
-      this.attack = 40;
-      this.defence = 10;
-    } else if (value == "Magician") {
-      this.attack = 10;
-      this.defence = 40;
-    } else if (value == "Daemon") {
-      this.attack = 10;
-      this.defence = 40;
-    } else if (value == "Undead") {
-      this.attack = 25;
-      this.defence = 25;
-    } else if (value == "Zombie") {
-      this.attack = 40;
-      this.defence = 10;
-    } else {
-      throw new Error("wrong type");
-    }
-    this._type = value;
-  }
+test("проверка соответствия объекта", () => {
+  expect(hero).toEqual({
+    _name: "OOP",
+    _type: "Magician",
+    attack: 10,
+    defence: 40,
+    health: 100,
+    level: 1,
+  });
+});
 
-  get type() {
-    return this._type;
-  }
+test("проверка повышения уровня", () => {
+  hero.levelUp();
+  expect(hero.level).toBe(2);
+});
 
-  levelUp() {
-    if (this.health > 0) {
-      this.level++;
-      this.attack *= 1.2;
-      this.health = 100;
-    } else {
-      throw new Error("you are dead");
-    }
-  }
-
-  damage(points) {
-    if (this.health > 0) {
-      this.health -= points * (1 - this.defence / 100);
-    }
-  }
-}
-
-// const unit = new Character("O2", "Magician");
-// console.log(unit);
-
-export default Character;
+test("проверка урона", () => {
+  hero.damage(10);
+  expect(hero.health).toBe(94);
+});
